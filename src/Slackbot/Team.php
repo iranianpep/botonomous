9e8226112b437aaaea1d91a4a886a7b5f2220881
Ehsan Abbasi <ehsan.abbasi@movember.com>
2017-01-09 10:54:46 +1100

    Add Team class

--- src/Slackbot/Team.php
+++ src/Slackbot/Team.php
@@ -2,7 +2,95 @@
 
 namespace Slackbot;
 
+/**
+ * Class Team.
+ * @package Slackbot
+ */
 class Team
 {
+    private $slackId;
+    private $name;
+    private $domain;
+    private $emailDomain;
+    private $icon;
 
+    /**
+     * @return string
+     */
+    public function getSlackId()
+    {
+        return $this->slackId;
+    }
+
+    /**
+     * @param string $slackId
+     */
+    public function setSlackId($slackId)
+    {
+        $this->slackId = $slackId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+    /**
+     * @param string $domain
+     */
+    public function setDomain($domain)
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmailDomain()
+    {
+        return $this->emailDomain;
+    }
+
+    /**
+     * @param string $emailDomain
+     */
+    public function setEmailDomain($emailDomain)
+    {
+        $this->emailDomain = $emailDomain;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIcon()
+    {
+        return $this->icon;
+    }
+
+    /**
+     * @param array $icon
+     */
+    public function setIcon(array $icon)
+    {
+        $this->icon = $icon;
+    }
 }
