5a05b31e04a0800c347aba293b9c307b0bf40801
Ehsan Abbasi <iranianpep@gmail.com>
2017-01-12 20:45:48 +1100

    Add OAuth class

---
+++ src/Slackbot/OAuth.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Slackbot;
+
+/**
+ * Class OAuth
+ * @package Slackbot
+ */
+class OAuth
+{
+    const AUTHORIZATION_URL = 'https://slack.com/oauth/authorize';
+
+    private $clientId;
+    private $scopes;
+    private $redirectUri;
+    private $state;
+    private $teamId;
+
+    /**
+     * OAuth constructor.
+     *
+     * @param       $clientId
+     * @param array $scopes
+     */
+    public function __construct($clientId, array $scopes)
+    {
+        $this->setClientId($clientId);
+        $this->setScopes($scopes);
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientId()
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * @param string $clientId
+     */
+    public function setClientId($clientId)
+    {
+        $this->clientId = $clientId;
+    }
+
+    /**
+     * @return array
+     */
+    public function getScopes()
+    {
+        return $this->scopes;
+    }
+
+    /**
+     * @param array $scopes
+     */
+    public function setScopes($scopes)
+    {
+        $this->scopes = $scopes;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRedirectUri()
+    {
+        return $this->redirectUri;
+    }
+
+    /**
+     * @param string $redirectUri
+     */
+    public function setRedirectUri($redirectUri)
+    {
+        $this->redirectUri = $redirectUri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $state
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTeamId()
+    {
+        return $this->teamId;
+    }
+
+    /**
+     * @param int $teamId
+     */
+    public function setTeamId($teamId)
+    {
+        $this->teamId = $teamId;
+    }
+
+    /**
+     * @param string $height
+     * @param string $weight
+     * @param string $cssClass
+     *
+     * @return string
+     */
+    public function generateAddButton($height = '40', $weight = '139', $cssClass = '')
+    {
+        $authorizationUrl = self::AUTHORIZATION_URL;
+        $scope = implode(',', $this->getScopes());
+        $clientId = $this->getClientId();
+
+        if (!empty($cssClass)) {
+            $cssClass = "class={$cssClass}";
+        } else {
+            $cssClass = '';
+        }
+
+        $html = "<a href='{$authorizationUrl}?scope={$scope}&client_id={$clientId}'>
+<img {$cssClass} alt='Add to Slack' height='{$height}' width='{$weight}'
+src='https://platform.slack-edge.com/img/add_to_slack.png'
+srcset='https://platform.slack-edge.com/img/add_to_slack.png 1x,
+https://platform.slack-edge.com/img/add_to_slack@2x.png 2x' /></a>";
+
+        return $html;
+    }
+}
