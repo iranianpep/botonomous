09ef3f7c913bfaa7812a1fdc7376a580bfe194e4
Ehsan Abbasi <iranianpep@gmail.com>
2017-01-14 21:38:56 +1100

    Add security utility

---
+++ src/Slackbot/utility/SecurityUtility.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Slackbot\utility;
+
+class SecurityUtility
+{
+    const DEFAULT_HASH_ALGORITHM = 'sha1';
+
+    private $hashAlgorithm;
+
+    /**
+     * generate a token.
+     *
+     * @return string
+     */
+    public function generateToken()
+    {
+        return hash($this->getHashAlgorithm(), uniqid(mt_rand(), true));
+    }
+
+    /**
+     * @param $hashAlgorithm
+     *
+     * @throws \Exception
+     */
+    public function setHashAlgorithm($hashAlgorithm)
+    {
+        /*
+         * check if hashing algorithm is valid
+         */
+        if (!in_array($hashAlgorithm, hash_algos(), true)) {
+            throw new \Exception('Hash algorithm is not valid');
+        }
+
+        $this->hashAlgorithm = $hashAlgorithm;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getHashAlgorithm()
+    {
+        if (empty($this->hashAlgorithm)) {
+            $this->setHashAlgorithm(self::DEFAULT_HASH_ALGORITHM);
+        }
+
+        return $this->hashAlgorithm;
+    }
+}
---
+++ src/tests/SecurityUtilityTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Slackbot\Tests;
+
+
+use Slackbot\utility\SecurityUtility;
+
+class SecurityUtilityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test generateToken.
+     */
+    public function testGenerateToken()
+    {
+        // hash function with the same algorithm always returns a value with the same length
+        $securityUtility = new SecurityUtility();
+        $token = $securityUtility->generateToken();
+        $result = hash($securityUtility->getHashAlgorithm(), 'something dummy');
+
+        $this->assertEquals(strlen($result), strlen($token));
+    }
+
+    /**
+     * Test getHashAlgorithm
+     *
+     * @throws \Exception
+     */
+    public function testGetHashAlgorithm()
+    {
+        $securityUtility = new SecurityUtility();
+
+        $this->assertEquals($securityUtility::DEFAULT_HASH_ALGORITHM, $securityUtility->getHashAlgorithm());
+
+        $securityUtility->setHashAlgorithm('sha256');
+        $this->assertEquals('sha256', $securityUtility->getHashAlgorithm());
+
+        $this->setExpectedException(
+            'Exception',
+            'Hash algorithm is not valid'
+        );
+
+        $securityUtility->setHashAlgorithm('');
+        $this->assertEquals($securityUtility::DEFAULT_HASH_ALGORITHM, $securityUtility->getHashAlgorithm());
+    }
+}
