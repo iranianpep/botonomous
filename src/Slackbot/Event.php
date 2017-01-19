ad1390cae641219b27872b7029f730d073a964e6
Ehsan Abbasi <iranianpep@gmail.com>
2017-01-19 23:10:38 +1100

    Add Event and EventListener

---
+++ src/Slackbot/Event.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Slackbot;
+
+class Event
+{
+    private $type;
+    private $user;
+    private $text;
+    private $timestamp;
+    private $eventTimestamp;
+    private $channel;
+
+    public function __construct($type)
+    {
+        $this->setType($type);
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param string $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @param string $timestamp
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventTimestamp()
+    {
+        return $this->eventTimestamp;
+    }
+
+    /**
+     * @param string $eventTimestamp
+     */
+    public function setEventTimestamp($eventTimestamp)
+    {
+        $this->eventTimestamp = $eventTimestamp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+
+    /**
+     * @param string $channel
+     */
+    public function setChannel($channel)
+    {
+        $this->channel = $channel;
+    }
+}
---
+++ src/Slackbot/EventListener.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Slackbot;
+
+use Slackbot\utility\StringUtility;
+
+/**
+ * Class EventListener.
+ *
+ * @package Slackbot
+ */
+class EventListener
+{
+    private $request;
+    private $token;
+    private $teamId;
+    private $apiAppId;
+    private $event;
+    private $requestEventMaps = [
+        'ts' => 'timestamp',
+        'event_ts' => 'eventTimestamp'
+    ];
+
+    public function __construct($request)
+    {
+        $this->setRequest($request);
+        $this->loadEvent();
+    }
+
+    /**
+     * @return array
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * @param array $request
+     */
+    public function setRequest(array $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTeamId()
+    {
+        return $this->teamId;
+    }
+
+    /**
+     * @param string $teamId
+     */
+    public function setTeamId($teamId)
+    {
+        $this->teamId = $teamId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiAppId()
+    {
+        return $this->apiAppId;
+    }
+
+    /**
+     * @param string $apiAppId
+     */
+    public function setApiAppId($apiAppId)
+    {
+        $this->apiAppId = $apiAppId;
+    }
+
+    /**
+     * @return Event
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function setEvent(Event $event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function loadEvent()
+    {
+        $args = $this->getRequest();
+
+        if (!isset($args['type'])) {
+            throw new \Exception('Event type must be specified');
+        }
+
+        // create the event
+        $eventObject = new Event($args['type']);
+
+        // exclude type from the args since it's already passed
+        unset($args['type']);
+
+        $stringUtility = new StringUtility();
+        foreach ($args as $argKey => $argValue) {
+            if (array_key_exists($argKey, $this->requestEventMaps)) {
+                $argKey = $this->requestEventMaps[$argKey];
+            }
+
+            $setterName = 'set'.$stringUtility->snakeCaseToCamelCase($argKey);
+
+            // ignore calling the method if setter does not exist
+            if (!method_exists($eventObject, $setterName)) {
+                continue;
+            }
+
+            $eventObject->$setterName($argValue);
+        }
+
+        // set it
+        $this->setEvent($eventObject);
+    }
+}
