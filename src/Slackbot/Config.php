8eea373be5080d8e71e1a15f33f34ffa1e3edb84
Ehsan Abbasi <iranianpep@gmail.com>
2017-02-23 10:08:15 +0000

    Apply fixes from StyleCI

--- src/Slackbot/Config.php
+++ src/Slackbot/Config.php
@@ -52,6 +52,6 @@ class Config extends AbstractConfig
         'verificationToken' => 'YOUR_APP_VERIFICATION_TOKEN',
         'apiAppId'          => 'YOUR_API_ID',
         // This is mainly used for testing
-        'respondOk'         => true
+        'respondOk'         => true,
     ];
 }
