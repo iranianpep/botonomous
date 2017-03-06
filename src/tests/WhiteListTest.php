883e94017335d8fdac5f2360f226b066d6352683
Ehsan Abbasi <iranianpep@gmail.com>
2017-03-07 08:04:00 +1100

    new test

--- src/tests/WhiteListTest.php
+++ src/tests/WhiteListTest.php
@@ -37,6 +37,85 @@ class WhiteListTest extends \PHPUnit_Framework_TestCase
         $dictionaryData = [
             'access-control' => [
                 'whitelist' => [
+                    'userId' => []
+                ],
+            ]
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // set the dictionary
+        $whitelist->setDictionary($dictionary);
+
+        // since user id is not specified the result is NULL
+        $this->assertEmpty($whitelist->isUsernameWhiteListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'whitelist' => [
+                    'username' => [
+                        'dummyUserName'
+                    ],
+                    'userId' => [
+                        'dummyUserId'
+                    ]
+                ],
+            ]
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is not empty
+        $this->assertTrue($whitelist->isUsernameWhiteListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'whitelist' => [
+                    'username' => [],
+                    'userId' => []
+                ],
+            ]
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is set but is empty
+        $this->assertFalse($whitelist->isUsernameWhiteListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'whitelist' => [
+                    'username' => [
+                        'blahblah'
+                    ],
+                    'userId' => [
+                        'blahblah'
+                    ]
+                ],
+            ]
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is set but with a different user id
+        $this->assertFalse($whitelist->isUsernameWhiteListed());
+    }
+
+    public function testIsUserIdWhiteListed()
+    {
+        // set the dummy request
+        $request = [
+            'user_name' => 'dummyUserName',
+            'user_id' => 'dummyUserId'
+        ];
+
+        $whitelist = new WhiteList($request);
+
+        // load the dictionary with dummy data
+        $dictionary = new Dictionary();
+        $dictionaryData = [
+            'access-control' => [
+                'whitelist' => [
                     'username' => []
                 ],
             ]
