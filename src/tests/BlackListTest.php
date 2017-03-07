07908ca29d6bcef7e0bf73cba804ba131ad2f3e3
Ehsan Abbasi <iranianpep@gmail.com>
2017-03-08 01:32:35 +1100

    resolve conflicts

--- src/tests/BlackListTest.php
+++ src/tests/BlackListTest.php
@@ -216,11 +216,7 @@ class BlackListTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(true, $blacklist->isBlackListed());
     }
 
-<<<<<<< HEAD
     public function testIsBlackListedUserId()
-=======
-    public function testIsBlackListed()
->>>>>>> a22e7d09ee1c127c8ffda1d75c7b9cc31d428e1d
     {
         $client = (new PhpunitHelper())->getUserInfoClient();
         $blacklist = $this->getBlackList();
@@ -256,22 +252,7 @@ class BlackListTest extends \PHPUnit_Framework_TestCase
             ],
         ]);
 
-<<<<<<< HEAD
         $blacklist = $this->getBlackList();
-=======
-        $blacklist->setDictionary($dictionary);
-
-        $this->assertEquals(true, $blacklist->isBlackListed());
-
-        $dictionary->setData([
-            'access-control' => [
-                'blacklist' => [
-                    'userEmail' => ['bobby@slack.com'],
-                ],
-            ],
-        ]);
-
->>>>>>> a22e7d09ee1c127c8ffda1d75c7b9cc31d428e1d
         $blacklist->setDictionary($dictionary);
 
         $this->assertEquals(true, $blacklist->isBlackListed());
