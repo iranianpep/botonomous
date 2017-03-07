fff264c6621a090dd404c13fe4c60eaf68cc97a5
Ehsan Abbasi <iranianpep@gmail.com>
2017-03-07 19:37:16 +1100

    add blacklist check test

---
+++ src/tests/BlackListTest.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Slackbot;
+
+/** @noinspection PhpUndefinedClassInspection */
+class BlackListTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsUsernameBlackListed()
+    {
+        // set the dummy request
+        $request = [
+            'user_name' => 'dummyUserName',
+            'user_id'   => 'dummyUserId',
+        ];
+
+        $blacklist = new BlackList($request);
+
+        // load the dictionary with dummy data
+        $dictionary = new Dictionary();
+        $dictionaryData = [
+            'access-control' => [
+                'blacklist' => [
+                    'userId' => [],
+                ],
+            ]
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // set the dictionary
+        $blacklist->setDictionary($dictionary);
+
+        // since user id is not specified the result is NULL
+        $this->assertEmpty($blacklist->isUsernameBlackListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'blacklist' => [
+                    'username' => [
+                        'dummyUserName'
+                    ],
+                    'userId' => [
+                        'dummyUserId'
+                    ]
+                ],
+            ]
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is not empty
+        $this->assertTrue($blacklist->isUsernameBlackListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'blacklist' => [
+                    'username' => [],
+                    'userId' => []
+                ],
+            ]
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is set but is empty
+        $this->assertFalse($blacklist->isUsernameBlackListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'blacklist' => [
+                    'username' => [
+                        'blahblah'
+                    ],
+                    'userId' => [
+                        'blahblah'
+                    ]
+                ],
+            ]
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is set but with a different user id
+        $this->assertFalse($blacklist->isUsernameBlackListed());
+    }
+
+    public function testIsUserIdBlackListed()
+    {
+        // set the dummy request
+        $request = [
+            'user_name' => 'dummyUserName',
+            'user_id' => 'dummyUserId'
+        ];
+
+        $blacklist = new BlackList($request);
+
+        // load the dictionary with dummy data
+        $dictionary = new Dictionary();
+        $dictionaryData = [
+            'access-control' => [
+                'blacklist' => [
+                    'username' => [],
+                ],
+            ],
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // set the dictionary
+        $blacklist->setDictionary($dictionary);
+
+        // since user id is not specified the result is NULL
+        $this->assertEmpty($blacklist->isUserIdBlackListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'blacklist' => [
+                    'username' => [],
+                    'userId'   => [
+                        'dummyUserId',
+                    ],
+                ],
+            ],
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is not empty
+        $this->assertTrue($blacklist->isUserIdBlackListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'blacklist' => [
+                    'username' => [],
+                    'userId'   => [],
+                ],
+            ],
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is set but is empty
+        $this->assertFalse($blacklist->isUserIdBlackListed());
+
+        $dictionaryData = [
+            'access-control' => [
+                'blacklist' => [
+                    'username' => [],
+                    'userId'   => [
+                        'blahblah',
+                    ],
+                ],
+            ],
+        ];
+
+        $dictionary->setData($dictionaryData);
+
+        // user id is set but with a different user id
+        $this->assertFalse($blacklist->isUserIdBlackListed());
+    }
+}
