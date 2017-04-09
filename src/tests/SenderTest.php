8a0aa3eb5b81bec9bf1717b41cfbb89a5f3e7d3b
Ehsan Abbasi <iranianpep@gmail.com>
2017-04-09 18:08:48 +1000

    enhance Sender tests

--- src/tests/SenderTest.php
+++ src/tests/SenderTest.php
@@ -12,14 +12,9 @@ class SenderTest extends TestCase
 {
     const VERIFICATION_TOKEN = 'verificationToken';
 
-    public function testSend()
+    public function testSendDebug()
     {
         $config = new Config();
-
-        /**
-         * Overwrite the config
-         */
-
         $slackbot = new Slackbot($config);
 
         /**
@@ -27,7 +22,40 @@ class SenderTest extends TestCase
          */
         $request = [
             'token'     => $config->get(self::VERIFICATION_TOKEN),
-            'debug'     => true,
+            'user_id'   => 'dummyId',
+            'user_name' => 'dummyUsername',
+        ];
+
+        $request['debug'] = true;
+
+        // get listener
+        $listener = $slackbot->getListener();
+
+        // set request
+        $listener->setRequest($request);
+
+        $slackbot->setConfig($config);
+
+        $sender = new Sender($slackbot);
+
+        $sender->send('#general', 'test response');
+
+        $response = '{"text":"test response","channel":"#general"}';
+
+        $this->expectOutputString($response);
+    }
+
+    public function testSendSlackJson()
+    {
+        $config = new Config();
+        $config->set('response', 'json');
+        $slackbot = new Slackbot($config);
+
+        /**
+         * Overwrite the slackbot
+         */
+        $request = [
+            'token'     => $config->get(self::VERIFICATION_TOKEN),
             'user_id'   => 'dummyId',
             'user_name' => 'dummyUsername',
         ];
@@ -47,5 +75,20 @@ class SenderTest extends TestCase
         $response = '{"text":"test response","channel":"#general"}';
 
         $this->expectOutputString($response);
+    }
+
+    public function testGetConfig()
+    {
+        $config = new Config();
+        $originalChannel = $config->get('channel');
+        $config->set('channel', $originalChannel . 'Changed');
+
+        $sender = new Sender((new Slackbot()));
+        $sender->setConfig($config);
+
+        $this->assertEquals($config, $sender->getConfig());
+
+        // reset config value
+        $config->set('channel', $originalChannel);
     }
 }
