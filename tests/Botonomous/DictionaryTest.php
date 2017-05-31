9bae4687a00c7410a8f7fccfb275a75523a0253e
Ehsan Abbasi <ehsan.abbasi@movember.com>
2017-05-31 15:06:17 +1000

    Add tests for getValueByKey

--- tests/Botonomous/DictionaryTest.php
+++ tests/Botonomous/DictionaryTest.php
@@ -27,4 +27,37 @@ class DictionaryTest extends TestCase
         // get it again to check load only is called once
         $this->assertEquals($expected, $dictionary->get('test'));
     }
+
+    /**
+     * Test getValueByKey.
+     */
+    public function testGetValueByKey()
+    {
+        $dictionary = new Dictionary();
+
+        $this->assertEquals('testValue', $dictionary->getValueByKey('test-key-value', 'testKey'));
+    }
+
+    /**
+     * Test getValueByKey.
+     */
+    public function testGetValueByInvalidKey()
+    {
+        $dictionary = new Dictionary();
+
+        $this->expectException('\Exception');
+        $this->expectExceptionMessage("Key: 'testInvalidKey' does not exist in file: test-key-value");
+
+        $this->assertEquals('testValue', $dictionary->getValueByKey('test-key-value', 'testInvalidKey'));
+    }
+
+    /**
+     * Test getValueByKey.
+     */
+    public function testGetValueByKeyWithReplacements()
+    {
+        $dictionary = new Dictionary();
+
+        $this->assertEquals('testValue dummy user', $dictionary->getValueByKey('test-key-value', 'testKeyWithReplacements', ['user' => 'dummy user']));
+    }
 }
